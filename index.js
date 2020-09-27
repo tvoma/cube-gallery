eb95f76b8c4074f7da5c4a2c4d56f5bb17d30b45
TyomaMtl <scollioudfree.fr@gmail.com>
2020-09-27 18:37:52 +0200

    style(index): proper code

--- index.js
+++ index.js
@@ -8,54 +8,51 @@ class CubeGallery {
         this.unit = 'px'
         // margin
         this.margin = options && options.margin > 0 ? options.margin : 0
+        
+        // gallery container
+        this.gallery = document.querySelector('#' + this.id)
+        gallery.style.fontSize = '0' // remove white spaces
+        gallery.style.lineHeight = '0' // remove white spaces
+
+        // gallery width
+        this.galleryWidth = this.gallery.offsetWidth
+
+        // images
+        this.images = document.querySelectorAll('#' + this.id + ' img')
+        this.images.forEach(img => {
+            img.width = Math.floor(img.naturalWidth * this.minHeight / img.naturalHeight), // default width
+            img.height = this.minHeight // default height
+        })
+        // count images
+        this.nbImages = this.images.length
     }
 }
 
-CubeGallery.prototype.create = function () {
-    // gallery container
-    const gallery = document.querySelector('#' + this.id)
-    // remove white spaces
-    gallery.style.fontSize = '0'
-    gallery.style.lineHeight = '0'
-
-    
-    // gallery width
-    const galleryWidth = gallery.offsetWidth
-    
-    // images
-    const images = document.querySelectorAll('#' + this.id + ' img')
-    images.forEach(img => {
-        img.width = Math.floor(img.naturalWidth * this.minHeight / img.naturalHeight), // default width
-        img.height = this.minHeight // default height
-    })
-
-    // generate rows
-    var nbImgs = images.length
+CubeGallery.prototype.create = function () { 
     var rows = []
-    var row = []
-    var sum = 0
-    for (let i = 0; i < nbImgs; i++) {
-        let current = images[i]
-        let next = images[i+1] != undefined ? images[i+1] : null
+    var imgs = []
+    var sumOfWidth = 0
+    for (let i = 0; i < this.nbImages; i++) {
+        let currentImg = this.images[i]
+        let nextImg = this.images[i+1] != undefined ? this.images[i+1] : null
 
-        sum = Math.floor(sum + current.width)
+        sumOfWidth = Math.floor(sumOfWidth + currentImg.width)
 
-        row.push(current)
+        imgs.push(currentImg)
 
-        if (next == null || Math.floor(sum + next.width) > galleryWidth) {
-            rows.push(row)
-            sum = 0
-            row = []
+        if (nextImg == null || Math.floor(sumOfWidth + nextImg.width) > this.galleryWidth) {
+            rows.push(imgs)
+            sumOfWidth = 0
+            imgs = []
         }
     }
 
     rows.forEach(imgs => {
-        let sum = 0 // sum of the width of the images
-        imgs.forEach(img => sum = Math.floor(sum + img.width))
+        let sumOfWidth = 0 // sum of the width of the images
+        imgs.forEach(img => sumOfWidth = Math.floor(sumOfWidth + img.width))
         imgs.forEach(img => {
-            img.width = Math.floor((img.width * (img.height * galleryWidth / sum) / img.height) - this.margin * 2)
-            img.height = Math.floor((img.height * galleryWidth / sum) - this.margin * 2)
-            img.style.height = img.height + this.unit // set image height
+            img.width = Math.floor((img.width * (img.height * this.galleryWidth / sumOfWidth) / img.height) - this.margin * 2)
+            img.height = Math.floor((img.height * this.galleryWidth / sumOfWidth) - this.margin * 2)
             img.style.margin = this.margin + 'px'
         })
     })
